d158c395c19be7a5f7297eb319c4aa2a818b352f
eladav <eladav86@gmail.com>
2015-08-06 17:41:00 +0300

    Show real images from Facebook and order them nicely

--- FacebookPhotoKeyboard/FacebookPhotosCustomKeyboard/KeyboardViewController.swift
+++ FacebookPhotoKeyboard/FacebookPhotosCustomKeyboard/KeyboardViewController.swift
@@ -11,10 +11,11 @@ import FBSDKLoginKit
 import PINRemoteImage
 import SwiftyJSON
 
-class KeyboardViewController: UIInputViewController {
+class KeyboardViewController: UIInputViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
     @IBOutlet var nextKeyboardButton: UIButton!
-    var imageView : UIImageView!
+    var photos : JSON!
+    var collectionView : UICollectionView!
     override func updateViewConstraints() {
         super.updateViewConstraints()
         
@@ -38,24 +39,26 @@ class KeyboardViewController: UIInputViewController {
         var nextKeyboardButtonLeftSideConstraint = NSLayoutConstraint(item: self.nextKeyboardButton, attribute: .Left, relatedBy: .Equal, toItem: self.view, attribute: .Left, multiplier: 1.0, constant: 0.0)
         var nextKeyboardButtonBottomConstraint = NSLayoutConstraint(item: self.nextKeyboardButton, attribute: .Bottom, relatedBy: .Equal, toItem: self.view, attribute: .Bottom, multiplier: 1.0, constant: 0.0)
         self.view.addConstraints([nextKeyboardButtonLeftSideConstraint, nextKeyboardButtonBottomConstraint])
-        
-        self.imageView = UIImageView()
+        var flowLayout = UICollectionViewFlowLayout()
+        flowLayout.scrollDirection = UICollectionViewScrollDirection.Horizontal
+        flowLayout.minimumLineSpacing = 10000.0
+        self.collectionView = UICollectionView(frame: self.view.bounds, collectionViewLayout: flowLayout)
+        self.collectionView.registerClass(ImageCollectionViewCell.self, forCellWithReuseIdentifier: "cell_identifier")
+        self.collectionView.dataSource = self
+        self.collectionView.delegate = self
+        self.view.addSubview(self.collectionView)
         
         let userDefaults = NSUserDefaults(suiteName: "group.com.rababa.FacebookPhotoKeyboard")
         
         if let rawPhotosData = userDefaults?.valueForKey("user_photos") {
-            let photos = JSON(rawPhotosData)
-            println(photos)
-            if let imageUrlString = photos["data"][0]["images"][5]["source"].string {
-                self.imageView.pin_setImageFromURL(NSURL(string: imageUrlString))
-            }
+            self.photos = JSON(rawPhotosData)["data"]
             
         }
-        self.view.addSubview(self.imageView)
     }
     
     override func viewWillLayoutSubviews() {
-        self.imageView.frame = self.view.bounds
+        super.viewWillLayoutSubviews()
+        self.collectionView.frame = self.view.bounds
     }
     
     override func didReceiveMemoryWarning() {
@@ -80,4 +83,73 @@ class KeyboardViewController: UIInputViewController {
         self.nextKeyboardButton.setTitleColor(textColor, forState: .Normal)
     }
     
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.photos.count
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        var cell : ImageCollectionViewCell = collectionView.dequeueReusableCellWithReuseIdentifier("cell_identifier", forIndexPath: indexPath) as! ImageCollectionViewCell
+
+        let currentImages = photos[indexPath.row]["images"]
+        let imageIndex = getBestFittingPhoto(currentImages)
+//        cell.backgroundColor = UIColor.redColor()
+        
+        cell.configureForDisplay(NSURL(string: currentImages[imageIndex]["source"].string!))
+        return cell
+    }
+    
+    func getBestFittingPhoto(images : JSON) -> Int {
+        var minimumHeightDiff = Float(Int.max)
+        var indexOfMinimum = 0
+        for (index,element) in enumerate(images.array!) {
+            var currentHeightDiff = abs(Float(self.view.bounds.height) - element["height"].float!)
+            if currentHeightDiff < minimumHeightDiff {
+                minimumHeightDiff = currentHeightDiff
+                indexOfMinimum = index
+            }
+        }
+        return indexOfMinimum
+    }
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+
+        var images = photos[indexPath.row]["images"]
+        let width = CGFloat(images[getBestFittingPhoto(images)]["width"].float!)
+        let height = self.view.bounds.height - 10
+        return CGSizeMake(width, height)
+    }
+    
+}
+
+
+
+class ImageCollectionViewCell : UICollectionViewCell {
+    var imageView : UIImageView!
+    var cacheImageView : UIImageView!
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.imageView = UIImageView(frame: frame)
+    }
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+
+    }
+    
+    func configureForDisplay(url : NSURL!) {
+        let imageView = UIImageView()
+        imageView.pin_setImageFromURL(url, completion: { (result) -> Void in
+            self.imageView.image = result.image
+            self.contentView.addSubview(self.imageView)
+            self.imageView.frame = self.contentView.bounds
+            self.contentView.setNeedsLayout()
+        })
+        self.cacheImageView = imageView
+    }
+    
+    override func prepareForReuse() {
+        self.cacheImageView = nil
+        for subview in Array(self.contentView.subviews) {
+            subview.removeFromSuperview()
+        }
+    }
 }
